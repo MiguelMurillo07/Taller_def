3b9f9a42f0a057b983d56e0b767b99a158360b5b
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2022-10-15 08:53:59 -0500

    Continuación 3er programa

--- parametro_lista_sumadatos.py
+++ parametro_lista_sumadatos.py
@@ -1,1 +1,13 @@
 # Definición de la función
+
+def sumar_datos(lista):
+    suma = 0
+    for i in lista:
+        suma = suma + i
+    return suma
+
+
+#lista 
+lista1 = [2, 3, 4, 5, 6]
+suma = sumar_datos(lista1)
+print(suma)
