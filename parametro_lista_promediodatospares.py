09d8e9578a41f461a73e505a0896f01a69b9b7df
José Miguel Murillo Cediel <111622978+MiguelMurillo07@users.noreply.github.com>
2022-12-01 07:51:38 -0500

    Update parametro_lista_promediodatospares.py

--- parametro_lista_promediodatospares.py
+++ parametro_lista_promediodatospares.py
@@ -1,1 +1,14 @@
 # Definición de la funcion
+
+
+def sumar_datos(lista):
+    suma = 0
+    for i in lista:
+        suma = suma + i
+    return suma
+
+
+#lista
+lista1 = [2,3,4,5,6]
+suma = sumar_datos(lista1)
+print(suma)
